676d4602fc249e351e33a8ae49d55e1aa8c31dc4
pschake34 <pschake34@charlottesvilleschools.org>
2020-11-20 09:24:59 -0500

    Update code

--- led_fade/led_fade.ino
+++ led_fade/led_fade.ino
@@ -4,14 +4,13 @@
 // The sine function which controls the brightness can be controlled by manipulating the variables below.
 
 //define constants
-#define ledPin 9    //the LED pin
-#define offset 128    //moves the value up 128 to account for negative #s coming from sin()
-#define amplitude 127   //used to amplify the number coming from sin()
-#define length 30   //controls the length of the sine wave
+const int ledPin = 9;
+const int offset = 128;    //moves the value up 128 to account for negative #s coming from sin()
+const int amplitude = 127;   //used to amplify the number coming from sin()
+const int length = 30;   //controls the length of the sine wave
 
-int counter = 0;    //advances the x-value
-int brightness;   //declare the variable for LED brightness
-
+int counter = 0;    //used to advance the x-value of the sine wave
+int brightness;
 
 void setup() {
   Serial.begin(9600);
@@ -19,15 +18,15 @@ void setup() {
 }
 
 void loop() {
-  analogWrite(ledPin, brightness);   //set the LED brightness
-  counter++;    //advance the counter
+  analogWrite(ledPin, brightness);
+  counter++;
   brightness = offset + amplitude * sin((TWO_PI / length) * counter);   //change brightness for next loop
   visualizeBrightness(brightness);    //visualizes the brightness in the Serial monitor
   delay(150);   //adds delay so the fade happens gradually
 }
 
 void visualizeBrightness(int brightness) {
-  brightness = brightness / 6;   //make brightness val smaller
+  brightness = brightness / 6;   //make brightness val smaller so the graph isn't huge
   for (int i = 0; i < brightness; i++) {    //print "=" for every point of brightness
     Serial.print("=");
   }
